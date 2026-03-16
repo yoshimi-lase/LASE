198b2e2395b552e1252cbc3279f6e27565e2d590
yoshimi-lase <yoshimi.nakano@gmail.com>
2026-03-16 11:23:07 +0900

    Delete laser_optics_mobile_app_pwa directory

--- laser_optics_mobile_app_pwa/sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('laser-optics-v1').then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
